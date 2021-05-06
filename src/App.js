ca9c6018a2ab074e6963e93706d2e2c86adc65dc
Ignacio Elías Rosner <ignaciorosner@gmail.com>
2021-05-06 15:40:22 -0300

    Updated the front end project after sketching.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import "./App.css";
-import Dashboard from "./components/dashboard/Dashboard.js";
-import { BrowserRouter as Router, Route, Redirect, Link, Switch } from "react-router-dom";
-import React from "react";
-
-//App Component is the main component in React which acts as a container for all other components
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/admin" component={Dashboard} />
-          <Redirect from="/" to="/admin/Home"></Redirect>
-        </Switch>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-//The following tells that App Component has to be loaded into the div element present in index.html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- 
-    <App />, /* The various pages will be displayed by the `Main` component. */
- 
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
